cc44c7b43faff470d93165fc890e2326a81d6c17
yutanj <yuta.bstars@gmail.com>
2023-10-18 13:15:07 +0900

    first commit

---
+++ src/main/java/com/example/cerespr/CeresPrApplication.java
@@ -0,0 +1,14 @@
+package com.example.cerespr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeresPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeresPrApplication.class, args);
+        //comment
+    }
+
+}
---
+++ src/test/java/com/example/cerespr/CeresPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cerespr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeresPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
